4cc548ad399d52d45276b6015bdf1f5d1d6f3a74
MubarakMDM <md93150@gmail.com>
2024-07-01 19:24:33 +0200

    second verion v2

--- src/data_integration.py
+++ src/data_integration.py
@@ -4,4 +4,5 @@ import os
 
 df = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/master/Ecommerce%20Customers.csv')
 df =df.iloc[:,3:]
+df=df['Avg. Session Length'].sample(200)
 df.to_csv(os.path.join('data','customer.csv'))
