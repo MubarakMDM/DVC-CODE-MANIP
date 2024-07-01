b006529be8abf5296e62b4dc1627db6589cdd664
MubarakMDM <md93150@gmail.com>
2024-07-01 19:15:53 +0200

    first verion 1

---
+++ src/data_integration.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import os
+
+df = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/master/Ecommerce%20Customers.csv')
+df =df.iloc[:,3:]
+df.to_csv(os.path.join('data','customer.csv'))
